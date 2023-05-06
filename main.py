0b23faac11a1cd72ebefb64a41ce9839794ded4c
Bernie Petters <bp4924@gmail.com>
2023-05-06 09:14:28 -0400

    added encodings for utf-8

--- main.py
+++ main.py
@@ -5,59 +5,63 @@ from urllib.parse import urljoin, urlparse
 
 file_path = 'results/urls.txt'
 
-delete_file = input("Do you want to delete the file? (y/n): ")  
+delete_file = input("Do you want to delete the file? (y/n): ")
 if delete_file == 'y':
     if os.path.exists(file_path):
         os.remove(file_path)
         print("File deleted")
     else:
         print("The file does not exist")
-        
+
+
 class SimpleSpider:
-   def __init__(self, start_url, max_pages=10):
-       self.start_url = start_url
-       self.max_pages = max_pages
-       self.visited_pages = set()
+    def __init__(self, start_url, max_pages=10):
+        self.start_url = start_url
+        self.max_pages = max_pages
+        self.visited_pages = set()
 
-   def is_valid_url(self, url):
-       return urlparse(url).scheme in ['http', 'https']
+    def is_valid_url(self, url):
+        return urlparse(url).scheme in ['http', 'https']
 
-   def get_links(self, url):
-       response = requests.get(url)
-       soup = BeautifulSoup(response.text, 'html.parser')
-       links = set()
-       
-       for link in soup.find_all('a', href=True):
-           href = link['href']
-           if self.is_valid_url(href):
-               links.add(href)
-           elif href.startswith('/'):
-               links.add(urljoin(url, href))
+    def get_links(self, url):
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links = set()
 
-       headers = soup.find_all(['h1', 'h2'])
-       with open(file_path, 'a') as f:
-           for header in headers:
-               f.write(header.text + '\n') 
+        for link in soup.find_all('a', href=True):
+            href = link['href']
+            if self.is_valid_url(href):
+                links.add(href)
+            elif href.startswith('/'):
+                links.add(urljoin(url, href))
 
-       return links
+        headers = soup.find_all(['h1', 'h2'])
+        with open(file_path, 'a', encoding='utf-8') as f:
+            for header in headers:
+                f.write(header.text + '\n')
 
-   def crawl(self, url):
-       if len(self.visited_pages) >= self.max_pages:
-           return
+#               with open('filename.txt', 'w', encoding='windows-1252') as f:
+#    f.write('Some text with special characters: é, ç, ñ')
 
-       if url not in self.visited_pages:
-           with open(file_path, 'a') as f:
+        return links
+
+    def crawl(self, url):
+        if len(self.visited_pages) >= self.max_pages:
+            return
+
+        if url not in self.visited_pages:
+            with open(file_path, 'a') as f:
                 f.write(url + '\n')
-           self.visited_pages.add(url)
+            self.visited_pages.add(url)
 #           print(f"Visiting: {url}")
-           links = self.get_links(url)
+            links = self.get_links(url)
 
-           for link in links:
-               self.crawl(link)
+            for link in links:
+                self.crawl(link)
+
 
 if __name__ == "__main__":
-   start_url = "https://example.com/"
-   spider = SimpleSpider(start_url)
-   spider.crawl(start_url)
-   print("Done")
-
+    start_url = "https://example.com/"
+    spider = SimpleSpider(start_url)
+    spider.crawl(start_url)
+    print("Done")
